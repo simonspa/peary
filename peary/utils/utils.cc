29c6bbcbccb73f9a4fda72ffa77f6b5cbdd248f1
Simon Spannagel <simon.spannagel@cern.ch>
2016-12-01 18:26:29 +0100

    Add configuration class, add utils required for configuration

--- peary/utils/utils.cc
+++ peary/utils/utils.cc
@@ -16,3 +16,14 @@ uint8_t caribou::reverseByte(uint8_t n) {
    */
    return (lookup[n&0b1111] << 4) | lookup[n>>4];
 }
+
+std::string caribou::trim(const std::string &s) {
+  static const std::string spaces = " \t\n\r\v";
+  size_t b = s.find_first_not_of(spaces);
+  size_t e = s.find_last_not_of(spaces);
+  if (b == std::string::npos || e == std::string::npos) {
+    return "";
+  }
+  return std::string(s, b, e - b + 1);
+}
+
