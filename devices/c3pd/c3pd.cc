aaa0f04d3a3859e1763c1ec53995b8d43c09e48a
Simon Spannagel <simon.spannagel@cern.ch>
2016-12-15 14:02:32 +0100

    Add empty C3PD device skeleton

---
+++ devices/c3pd/c3pd.cc
@@ -0,0 +1,39 @@
+/**
+ * Caribou implementation for the C3PD
+ */
+
+#include "c3pd.h"
+#include "hal.h"
+#include "log.h"
+
+using namespace caribou;
+
+C3PD::~C3PD() {
+  LOG(logINFO) << DEVICE_NAME << ": Shutdown, delete device.";
+}
+
+void C3PD::init() {
+  LOG(logINFO) << DEVICE_NAME << ": Initialized.";
+}
+
+void C3PD::powerOn() {
+  LOG(logINFO) << DEVICE_NAME << ": Power on.";
+}
+
+void C3PD::powerOff() {
+  LOG(logINFO) << DEVICE_NAME << ": Power off.";
+}
+
+void C3PD::daqStart() {
+  LOG(logINFO) << DEVICE_NAME << ": DAQ started.";
+}
+
+void C3PD::daqStop() {
+  LOG(logINFO) << DEVICE_NAME << ": DAQ stopped.";
+}
+
+caribouDevice* caribou::generator(const caribou::Configuration config) {
+  LOG(logDEBUG) << "Generator: " << DEVICE_NAME;
+  C3PD* mDevice = new C3PD(config);
+  return dynamic_cast<caribouDevice*>(mDevice);
+}
---
+++ devices/c3pd/c3pd.h
@@ -0,0 +1,55 @@
+/**
+ * Caribou implementation for the C3PDF chip
+ */
+
+#ifndef DEVICE_C3PD_H
+#define DEVICE_C3PD_H
+
+#include "device.h"
+
+namespace caribou {
+  /** C3PD Device class definition
+   */
+  class C3PD : public caribouDevice {
+    
+  public:
+  C3PD(const caribou::Configuration config) : caribouDevice(config) {};
+    ~C3PD();
+    
+    /** Initializer function for C3PD
+     */
+    void init();
+
+    /** Turn on the power supply for the C3PD chip
+     */
+    void powerOn();
+
+    /** Turn off the C3PD power
+     */
+    void powerOff();
+
+    /** Start the data acquisition
+     */
+    void daqStart();
+
+    /** Stop the data acquisition
+     */
+    void daqStop();
+
+  private:
+
+  };
+
+  /** Device generator
+   *
+   *  This generator function is used by the device manager to create instances of
+   *  this device using the abstract caribouDevice class interface. It has to be implemented
+   *  for every class deriving from caribouDevice.
+   */
+  extern "C" {
+    caribouDevice * generator(const caribou::Configuration);
+  }
+
+} //namespace caribou
+
+#endif /* DEVICE_C3PD_H */
