b8e8a4dd8a8285de54bde7f683260407c1d8f95d
Simon Spannagel <simon.spannagel@cern.ch>
2017-01-06 13:26:43 +0100

    Add implementations for integer conversion

--- peary/utils/utils.cc
+++ peary/utils/utils.cc
@@ -27,3 +27,48 @@ std::string caribou::trim(const std::string &s) {
   return std::string(s, b, e - b + 1);
 }
 
+template <> int64_t caribou::from_string(const std::string &x, const int64_t &def) {
+  if (x == "")
+    return def;
+  const char *start = x.c_str();
+  size_t end = 0;
+  int base = 10;
+  std::string bases("box");
+  if (x.length() > 2 && x[0] == '0' &&
+      bases.find(x[1]) != std::string::npos) {
+    if (x[1] == 'b')
+      base = 2;
+    else if (x[1] == 'o')
+      base = 8;
+    else if (x[1] == 'x')
+      base = 16;
+    start += 2;
+  }
+  int64_t result = static_cast<int64_t>(std::stoll(start, &end, base));
+  if (!x.substr(end).empty())
+    throw std::invalid_argument("Invalid argument: " + x);
+  return result;
+}
+
+template <> uint64_t caribou::from_string(const std::string &x, const uint64_t &def) {
+  if (x == "")
+    return def;
+  const char *start = x.c_str();
+  size_t end = 0;
+  int base = 10;
+  std::string bases("box");
+  if (x.length() > 2 && x[0] == '0' &&
+      bases.find(x[1]) != std::string::npos) {
+    if (x[1] == 'b')
+      base = 2;
+    else if (x[1] == 'o')
+      base = 8;
+    else if (x[1] == 'x')
+      base = 16;
+    start += 2;
+  }
+  uint64_t result = static_cast<uint64_t>(std::stoull(start, &end, base));
+  if (!x.substr(end).empty())
+    throw std::invalid_argument("Invalid argument: " + x);
+  return result;
+}
