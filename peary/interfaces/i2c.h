84c044689fc12b280bc202846ad3d126c62b4f91
Simon Spannagel <simon.spannagel@cern.ch>
2017-01-13 12:51:19 +0100

    Update I2C interface and add implementation

--- peary/interfaces/i2c.h
+++ peary/interfaces/i2c.h
@@ -3,28 +3,51 @@
 
 #include <vector>
 #include <cstdint>
+#include <mutex>
 
 namespace caribou {
 
-  class i2c {
+    /* I2C command interface class
+   */
+  class iface_i2c {
+
+    /** Default constructor: private for singleton class
+     */
+    iface_i2c() {};
 
   public:
-    /** Default constructor for creating a new I2C communicator
+    /** Get instance of the singleton I2C interface class
+     *  The below function is thread-safe in C++11 and can thus
+     *  be called from several HAL instances concurrently.
      */
-    i2c() {};
+    static iface_i2c * getInterface() {
+      static iface_i2c instance;
+      return &instance;
+    }
 
-    /** Default destructor for I2C objects
+    /* Delete unwanted functions from singleton class (C++11)
      */
-    ~i2c() {};
+    iface_i2c(iface_i2c const&)             = delete;
+    void operator=(iface_i2c const&)  = delete;
 
-    /** Send I2C command(s) to the address specified containing the
-     *  data supplied via the function argument
+    /** Send command via the I2C interface
+     *
+     *  The function accepts the register address to be written to as well as
+     *  a vector of data words to be sent. All data is send to the same address.
      */
     void sendCommand(uint8_t address, std::vector<uint8_t> data);
 
   private:
 
-  }; //class I2C
+    /** Sending a single I2C command and reading the return value
+     */
+    void sendCommand(uint8_t address, uint8_t data);
+
+    /** Private mutex to lock driver access
+     */
+    std::mutex mutex;
+    
+  }; //class iface_i2c
 
 } //namespace caribou
 
