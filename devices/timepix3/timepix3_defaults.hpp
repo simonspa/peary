334bf7a234fd6cd3158bee844f929ed279d51b39
Simon Spannagel <simon.spannagel@cern.ch>
2017-01-19 17:05:18 +0100

    Add defaults list for TPX3

---
+++ devices/timepix3/timepix3_defaults.hpp
@@ -0,0 +1,18 @@
+// Default register values for Timepix3 chips
+
+#define TPX3_VPREAMP_NCAS     128
+#define TPX3_IBIAS_IKRUM       10
+#define TPX3_VFBK             128
+#define TPX3_VTHRESH_FINE     168
+#define TPX3_VTHRESH_COARSE     6
+#define TPX3_IBIAS_DISCS1_ON  128
+#define TPX3_IBIAS_DISCS1_OFF   8
+#define TPX3_IBIAS_DISCS2_ON  128
+#define TPX3_IBIAS_DISCS2_OFF   8
+#define TPX3_IBIAS_PIXELDAC   128
+#define TPX3_IBIAS_TPBUFIN    128
+#define TPX3_IBIAS_TPBUFOUT   128
+#define TPX3_VTP_COARSE       128
+#define TPX3_VTP_FINE         256
+#define TPX3_IBIAS_CP_PLL     128
+#define TPX3_PLL_VCNTRL       128
