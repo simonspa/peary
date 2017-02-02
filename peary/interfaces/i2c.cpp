9eb76e91caf7362d1f26b1544da2feddeacc6c16
Adrian Fiergolski <adrian.fiergolski@cern.ch>
2017-02-02 19:37:46 +0100

    Rewrite i2c interface according to the new scheme.

--- peary/interfaces/i2c.cpp
+++ peary/interfaces/i2c.cpp
@@ -19,7 +19,7 @@
 
 using namespace caribou;
 
-iface_i2c::iface_i2c(std::string const & device_path) : devicePath(device_path) {
+iface_i2c::iface_i2c(std::string const & device_path) : Interface( device_path ) {
   if ( (i2cDesc = open( devicePath.c_str(), O_RDWR) ) < 0  ){
     throw DeviceException("Open " + device_path + " device failed. " + std::strerror(i2cDesc) );
   }
@@ -31,48 +31,91 @@ void iface_i2c::setAddress(i2c_address_t const address){
 			      ": " + std::strerror(errno) );
 }
 
-void iface_i2c::write(i2c_address_t const address, std::vector<i2c_data_t> & data) {
-
+std::vector<i2c_t> iface_i2c::write(const i2c_t& address, const i2c_t& data ){
   std::lock_guard<std::mutex> lock(mutex);
-
+  
   setAddress(address);
   
-  LOG(logINTERFACE) << std::hex << "I2C (" << devicePath <<") : Writing data \"";
-  for( auto i : data)
-    LOG(logINTERFACE) << static_cast<int>(i) << " ";
-  LOG(logINTERFACE) << "\" to addr \"" <<  static_cast<int>(address) << "\"" << std::dec << std::endl;
+  LOG(logINTERFACE) << std::hex << "I2C (" << devicePath <<") address " << static_cast<int>(address) << ": Writing data \"" << static_cast<int>(data)
+		    <<  "\"" << std::dec << std::endl;
 
-  if( ::write( i2cDesc, data.data(), data.size() ) != static_cast<ssize_t>( data.size() ) )
+  if( i2c_smbus_write_byte( i2cDesc, data) )
     throw CommunicationError( "Failed to write slave (" + to_hex_string(address) + ") on " + devicePath + ": " + std::strerror(errno) );
 }
 
-void iface_i2c::read(i2c_address_t const address, std::vector<i2c_data_t> & data,  const unsigned int length) {
-
-  std::lock_guard<std::mutex> lock(mutex);
-
-  setAddress(address);
-
-  data.resize(length);
-  
-  if( ::read( i2cDesc, data.data(), data.size() ) != static_cast<ssize_t>( data.size() ) )
-    throw CommunicationError( "Failed to read slave (" + to_hex_string(address) + ") on " + devicePath + ": " + std::strerror(errno) );
-  
-
-  LOG(logINTERFACE) << std::hex << "I2C (" << devicePath <<") : Read data \"";
-  for( auto i : data)
-    LOG(logINTERFACE) << static_cast<int>(i) << " ";
-  LOG(logINTERFACE) << "\" to addr \"" <<  static_cast<int>(address) << "\"" << std::dec << std::endl;
-
+std::vector<i2c_t> iface_i2c::write(const i2c_t& address, const std::vector<i2c_t>& data ){
+  throw CommunicationError( "Block write operation is not possible to a device without internal registers" );
 }
 
+std::vector<i2c_t> iface_i2c::write(const i2c_t& address,const std::pair<i2c_t, i2c_t> & data){
+
+  std::lock_guard<std::mutex> lock(mutex);
+  
+  setAddress(address);
+  
+  LOG(logINTERFACE) << std::hex << "I2C (" << devicePath <<") address " << static_cast<int>(address) << ": Register \"" << static_cast<int>(data.first)
+		    << "\" Writing data \"" << static_cast<int>(data.second) <<  "\"" << std::dec << std::endl;
 
 
-  virtual DATA_T& write(const ADDRESS_T& address,const DATA_T& data){
-    std::vector<DATA_T> data_v {data};
-    std::vector<DATA_T> ret = write(address, data_v);
-    switch( ret_size() ) {
-    case 0: return static_cast<DATA_T>( 0 );
-    case 1: return ret[0];
-    defualt:
-      throw  DataException( "For single access read more than 1 
-	}
+  if( i2c_smbus_write_byte_data( i2cDesc, data.first, data.second ) )
+    throw CommunicationError( "Failed to write slave (" + to_hex_string(address) + ") on " + devicePath + ": " + std::strerror(errno) );
+}
+  
+std::vector<i2c_t> iface_i2c::write(const i2c_t& address, const i2c_t & reg, const std::vector< i2c_t > & data){
+
+  std::lock_guard<std::mutex> lock(mutex);
+    
+  setAddress(address);
+
+  LOG(logINTERFACE) << std::hex << "I2C (" << devicePath <<") address " << static_cast<int>(address) << ": Register \"" << static_cast<int>(reg)
+		    << "\" Writing block data: \"";
+  for( auto i : data)
+    LOG(logINTERFACE) << static_cast<int>(i) << " ";
+  LOG(logINTERFACE) << "\""<< std::dec << std::endl;
+
+  if( i2c_smbus_write_block_data( i2cDesc, reg, data.size(), data.data() ) )
+    throw CommunicationError( "Failed to block write slave (" + to_hex_string(address) + ") on " + devicePath + ": " + std::strerror(errno) );
+}
+
+std::vector<i2c_t> iface_i2c::write(const i2c_t& address,const std::vector< std::pair<i2c_t, i2c_t> > & data){
+  throw CommunicationError( "Block write operation with different variate register address is not supported by ths I2C implementation" );
+}
+
+std::vector<<i2c_t> iface_i2c::read(const <i2c_t& address, const unsigned int& length) {
+  std::lock_guard<std::mutex> lock(mutex);
+  std::vector<<i2c_t> data;
+
+  if(length != 1)
+    throw CommunicationError( "Read operation of multiple data from the device without internal registers is not possible" );
+
+  setAddress(address);
+    
+  data.resize(1);
+
+  if( i2c_smbus_read_byte( i2cDesc ) )
+    throw CommunicationError( "Failed to read slave (" + to_hex_string(address) + ") on " + devicePath + ": " + std::strerror(errno) );
+
+  LOG(logINTERFACE) << std::hex << "I2C (" << devicePath <<") address " << static_cast<int>(address) << ": Read data \"" << static_cast<int>(data[0])
+		    <<  "\"" << std::dec << std::endl;
+  
+}
+
+std::vector<i2c_t> iface_i2c::read(const i2c_t& address, const i2c_t reg, const unsigned int& length = 1){
+
+  std::lock_guard<std::mutex> lock(mutex);
+  std::vector<<i2c_t> data;
+
+  setAddress(address);
+    
+  data.resize(length);
+
+  if( i2c_smbus_read_block_data( i2cDesc, reg, length, data.data() ) )
+    throw CommunicationError( "Failed to read slave (" + to_hex_string(address) + ") on " + devicePath + ": " + std::strerror(errno) );
+
+  LOG(logINTERFACE) << std::hex << "I2C (" << devicePath <<") address " << static_cast<int>(address) << ": Register \"" << static_cast<int>(reg)
+		    << "\" Read block data \"";
+  for( auto i : data)
+    LOG(logINTERFACE) << static_cast<int>(i) << " ";
+  LOG(logINTERFACE) << "\""<< std::dec << std::endl;
+
+}
--- peary/interfaces/i2c.hpp
+++ peary/interfaces/i2c.hpp
@@ -14,10 +14,9 @@ namespace caribou {
 
   
   
-  typedef uint16_t i2c_address_t;
-  typedef uint8_t  i2c_data_t;
+  typedef uint8_t i2c_t;
 
-  class iface_i2c {
+  class iface_i2c : public Interface<i2c_t, i2c_t>  {
 
   private:
 
@@ -34,9 +33,6 @@ namespace caribou {
     //Descriptor of the device
     int i2cDesc;
 
-    //Path of the device
-    const std::string devicePath;
-
     //Protects access to the bus
     std::mutex mutex;
 
@@ -45,44 +41,11 @@ namespace caribou {
     //Unused constructor
     iface_i2c()             = delete;
 
-
-    /** Write access via the I2C interface
-     *
-     *  The function accepts the register address to be written to as well as
-     *  a vector of data words to be sent. All data is send to the same address.
-     *
-     * It can throw CommunicationError
-     */
-    void write(i2c_address_t const address, std::vector<i2c_data_t> & data);
-
-    /** Read access via the I2C interface
-     *
-     *  The function accepts the register address to be read to, a destination vector of
-     *  data words and number of read bytes. All data is read from the same address.
-     *
-     * It can throw CommunicationError.
-     */
-    void read(i2c_address_t const address,  std::vector<i2c_data_t> & data, const unsigned int length);
-
-    
     //only this function can create the interface
-    friend iface_i2c* interface_manager<iface_i2c>::getInterface(std::string const & );
+    friend iface_i2c* interface_manager::getInterface< iface_i2c >(std::string const & );
 
   };
   
-  class I2C : interface_manager<iface_i2c> {
-  private:
-    
-    // Default constructor: private for singleton class 
-    I2C() {};
-    
-  public:
-    /* Delete unwanted functions from singleton class (C++11)
-     */
-    I2C(I2C const&) = delete;
-    void operator=(I2C const&)    = delete;
-  };
-
 
 }
 
