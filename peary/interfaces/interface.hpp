8460124919e9b865112a40e02cec2df993a2e93c
Adrian Fiergolski <adrian.fiergolski@cern.ch>
2017-02-02 19:36:31 +0100

    Define a basic interface.

--- peary/interfaces/interface.hpp
+++ peary/interfaces/interface.hpp
@@ -14,6 +14,11 @@
 template <typename ADDRESS_T = uint8_t, typename DATA_T = uint8_t, typename REG_T = DATA_T >
 virtual class Interface {
 
+  Interface( std::string devicePath) : devicePath(devicePath) {};
+  
+  //Path of the device
+  const std::string devicePath;
+  
 public: 
   //////////////////////
   // Write operations
@@ -21,35 +26,41 @@ public:
 
   //Write data to a device which does not contain internal register
   //If readout is intergralpart of write operations, the read values a returned by function. 
-  virtual std::vector<DATA_T>& write(const ADDRESS_T& address,const DATA_T& data){
+  virtual std::vector<DATA_T> write(const ADDRESS_T& address,const DATA_T& data){
     std::vector<DATA_T> data_v {data};
     return write(address, data_v);
   }
-  
+
   //Write data to a device which does not contain internal register
   //If readout is intergralpart of write operations, the read values a returned by function. 
-  std::vector<DATA_T>& write(const ADDRESS_T& address, const std::vector<DATA_T>& data) = 0;
+  std::vector<DATA_T> write(const ADDRESS_T& address, const std::vector<DATA_T>& data) = 0;
 
+  
   //Write data to a device containing internal registers
   //If readout is intergralpart of write operations, the read values a returned by function. 
-  virtual std::vector<DATA_T> & write(const ADDRESS_T& address,const std::pair<REG_T, DATA_T> & data){
+  virtual std::vector<DATA_T> write(const ADDRESS_T& address,const std::pair<REG_T, DATA_T> & data){
     std::vector<std::pair<REG_T, DATA_T> > data_v {data};
     return write(address, data_v);
   }
+
+  //Write data to a device containing internal registers
+  //If readout is intergralpart of write operations, the read values a returned by function. 
+  std::vector<DATA_T> write(const ADDRESS_T& address, const REG_T& reg, const std::vector< DATA_T>& data) = 0;
+
   
   //Write data to a device containing internal registers
   //If readout is intergralpart of write operations, the read values a returned by function. 
-  std::vector<DATA_T>& write(const ADDRESS_T& address,const std::vector<std::pair<REG_T, DATA_T> >& data) = 0;
+  std::vector<DATA_T> write(const ADDRESS_T& address,const std::vector<std::pair<REG_T, DATA_T> >& data) = 0;
 
   //////////////////////
   // Read operations
   //////////////////////
   
   //Read number of data words form the given device
-  std::vector<DATA_T>& read(const ADDRESS_T& address, const unsigned int& length) = 0;
+  std::vector<DATA_T> read(const ADDRESS_T& address, const unsigned int& length = 1) = 0;
 
   //Read number of data words form a register of the given device
-  std::vector<DATA_T>& read(const ADDRESS_T& address, const REG_T reg, const unsigned int& length) = 0;
+  std::vector<DATA_T> read(const ADDRESS_T& address, const REG_T reg, const unsigned int& length = 1) = 0;
 
 }
 
