d24d85a8bb8114fc8dbef0d1c726e75595d8ad91
Simon Spannagel <simon.spannagel@cern.ch>
2017-02-15 14:42:05 +0100

    Start adding CLICpix2 defaults and dictionary

--- devices/clicpix2/clicpix2_defaults.hpp
+++ devices/clicpix2/clicpix2_defaults.hpp
@@ -1,3 +1,17 @@
+#ifndef DEVICE_CLICPIX2_DEFAULTS_H
+#define DEVICE_CLICPIX2_DEFAULTS_H
 
+#include "carboard.hpp"
+#include "dictionary.hpp"
 
-#define DEFAULT_DEVICEPATH "/dev/i2c-0"
+namespace caribou {
+
+  /** Default device path for this device: SPI interface
+   */
+#define DEFAULT_DEVICEPATH "/dev/spi-0"
+
+  
+  
+} //namespace caribou
+
+#endif /* DEVICE_CLICPIX2_DEFAULTS_H */
