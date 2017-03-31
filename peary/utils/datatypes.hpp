b1483cf43813dff2d8189bc891b767d9552e973e
Simon Spannagel <simon.spannagel@cern.ch>
2017-03-31 11:42:37 +0200

    Move datatypes to caribou namespace, rewrok register class

--- peary/utils/datatypes.hpp
+++ peary/utils/datatypes.hpp
@@ -6,128 +6,158 @@
 
 #include <tuple>
 
-/** Component Configuration Base class
- */
-class component_t {
-public:
-  component_t(std::string name) : _name(name) {};
-  virtual ~component_t() {};
-  std::string name() const {return _name; };
-private:
-  std::string _name;
-};
+namespace caribou {
 
-/** Component Configuration class for components using a DAC output
- */
-class component_dac_t : public component_t {
-public:
-  component_dac_t(std::string name, uint8_t dacaddr, uint8_t dacout) :
-    component_t(name),
-    _dacaddress(dacaddr),
-    _dacoutput(dacout)
-  {};
-  virtual ~component_dac_t() {};
+  /** class to store a register configuration
+   *
+   *  @param address Address of the register in question
+   *  @param mask    Mask identifying which bits belong to the register.
+   *                 This allows to set registers which only occupy a
+   *                 fraction of the full-size register to be written
+   */
+  template<typename ADDR_T = uint8_t, typename MASK_T = uint8_t>
+  class registerConfig {
+  public:
+    registerConfig() : _address(0), _mask(0) {};
+    registerConfig(ADDR_T address, MASK_T mask) : _address(address), _mask(mask) {};
+    ADDR_T _address;
+    MASK_T _mask;
 
-  uint8_t dacaddress() const { return _dacaddress; };
-  uint8_t dacoutput() const { return _dacoutput; };
+    template<typename T>
+    friend std::ostream& operator<<(std::ostream& os, const registerConfig<T, T>& rg);
+  };
 
-private:
-  uint8_t _dacaddress;
-  uint8_t _dacoutput;
-};
+  template<typename T>
+  std::ostream& operator<<(std::ostream& os, const caribou::registerConfig<T, T>& rg) {
+    os << to_hex_string(rg._address) << " ("
+       << to_bit_string(rg._mask) << ")";
+    return os;  
+  }
+
+  /** Component Configuration Base class
+   */
+  class component_t {
+  public:
+    component_t(std::string name) : _name(name) {};
+    virtual ~component_t() {};
+    std::string name() const {return _name; };
+  private:
+    std::string _name;
+  };
+
+  /** Component Configuration class for components using a DAC output
+   */
+  class component_dac_t : public component_t {
+  public:
+    component_dac_t(std::string name, uint8_t dacaddr, uint8_t dacout) :
+      component_t(name),
+      _dacaddress(dacaddr),
+      _dacoutput(dacout)
+    {};
+    virtual ~component_dac_t() {};
+
+    uint8_t dacaddress() const { return _dacaddress; };
+    uint8_t dacoutput() const { return _dacoutput; };
+
+  private:
+    uint8_t _dacaddress;
+    uint8_t _dacoutput;
+  };
 
 
-/** Voltage Regulator Configuration
- *
- *  The parameters hold:
- *  - the name of the power output
- *  - the DAC address
- *  - the corresponding DAC output pin
- *  - the output pin of the power switch
- *  - the address of the current/power monitor
- */
-class VOLTAGE_REGULATOR_T : public component_dac_t {
-public:
-  VOLTAGE_REGULATOR_T(std::string name, uint8_t dacaddr, uint8_t dacout, uint8_t pwrswitch, uint8_t pwrmon) :
-    component_dac_t(name,dacaddr,dacout),
-    _powerswitch(pwrswitch),
-    _powermonitor(pwrmon)
-  {};
-  ~VOLTAGE_REGULATOR_T() {};
+  /** Voltage Regulator Configuration
+   *
+   *  The parameters hold:
+   *  - the name of the power output
+   *  - the DAC address
+   *  - the corresponding DAC output pin
+   *  - the output pin of the power switch
+   *  - the address of the current/power monitor
+   */
+  class VOLTAGE_REGULATOR_T : public component_dac_t {
+  public:
+    VOLTAGE_REGULATOR_T(std::string name, uint8_t dacaddr, uint8_t dacout, uint8_t pwrswitch, uint8_t pwrmon) :
+      component_dac_t(name,dacaddr,dacout),
+      _powerswitch(pwrswitch),
+      _powermonitor(pwrmon)
+    {};
+    ~VOLTAGE_REGULATOR_T() {};
 
-  uint8_t pwrswitch() const { return _powerswitch; };
-  uint8_t pwrmonitor() const { return _powermonitor; };
+    uint8_t pwrswitch() const { return _powerswitch; };
+    uint8_t pwrmonitor() const { return _powermonitor; };
   
-private:
-  uint8_t _powerswitch;
-  uint8_t _powermonitor;
-};
+  private:
+    uint8_t _powerswitch;
+    uint8_t _powermonitor;
+  };
 
 
-/** Current Source Configuration
- *
- *  The parameters hold:
- *  - the name of the current source
- *  - the DAC address
- *  - the corresponding DAC output pin
- *  - the output pin of the polarity switch
- */
-class CURRENT_SOURCE_T : public component_dac_t {
-public:
-  CURRENT_SOURCE_T(std::string name, uint8_t dacaddr, uint8_t dacout, uint8_t polswitch) :
-    component_dac_t(name,dacaddr,dacout),
-    _polswitch(polswitch)
-  {};
-  ~CURRENT_SOURCE_T() {};
+  /** Current Source Configuration
+   *
+   *  The parameters hold:
+   *  - the name of the current source
+   *  - the DAC address
+   *  - the corresponding DAC output pin
+   *  - the output pin of the polarity switch
+   */
+  class CURRENT_SOURCE_T : public component_dac_t {
+  public:
+    CURRENT_SOURCE_T(std::string name, uint8_t dacaddr, uint8_t dacout, uint8_t polswitch) :
+      component_dac_t(name,dacaddr,dacout),
+      _polswitch(polswitch)
+    {};
+    ~CURRENT_SOURCE_T() {};
 
-  uint8_t polswitch() const { return _polswitch; };
+    uint8_t polswitch() const { return _polswitch; };
   
-private:
-  uint8_t _polswitch;
-};
+  private:
+    uint8_t _polswitch;
+  };
 
-/** Slow ADC Channel Configuration
- *
- *  The parameters hold (in this order):
- *  - the channel number
- */
-class SLOW_ADC_CHANNEL_T : public component_t {
-public:
-  SLOW_ADC_CHANNEL_T(std::string name, uint8_t channel, uint8_t address) :
-    component_t(name),
-    _channel(channel),
-    _address(address)
-  {};
-  virtual ~SLOW_ADC_CHANNEL_T() {};
+  /** Slow ADC Channel Configuration
+   *
+   *  The parameters hold (in this order):
+   *  - the channel number
+   */
+  class SLOW_ADC_CHANNEL_T : public component_t {
+  public:
+    SLOW_ADC_CHANNEL_T(std::string name, uint8_t channel, uint8_t address) :
+      component_t(name),
+      _channel(channel),
+      _address(address)
+    {};
+    virtual ~SLOW_ADC_CHANNEL_T() {};
 
-  uint8_t channel() const { return _channel; };
-  uint8_t address() const { return _address; };
+    uint8_t channel() const { return _channel; };
+    uint8_t address() const { return _address; };
 
-private:
-  uint8_t _channel;
-  uint8_t _address;
-};
+  private:
+    uint8_t _channel;
+    uint8_t _address;
+  };
 
-/**  Bias Voltage Regulator Configuration
- *  
- *  - the name of the bias voltage
- *  - the DAC address
- *  - the corresponding DAC output pin
- */
-typedef component_dac_t BIAS_REGULATOR_T;
+  /**  Bias Voltage Regulator Configuration
+   *  
+   *  - the name of the bias voltage
+   *  - the DAC address
+   *  - the corresponding DAC output pin
+   */
+  // FIXME cannot do this, otherwise casting is ambiguous for the two!
+  typedef component_dac_t BIAS_REGULATOR_T;
 
-/**  Injection Bias Voltage Regulator Configuration
- *  
- *  - the name of the injection bias
- *  - the DAC address
- *  - the corresponding DAC output pin
- *  - FIXME: INJ_CTRL_X signals from FPGA!
- */
-typedef component_dac_t INJBIAS_REGULATOR_T;
+  /**  Injection Bias Voltage Regulator Configuration
+   *  
+   *  - the name of the injection bias
+   *  - the DAC address
+   *  - the corresponding DAC output pin
+   *  - FIXME: INJ_CTRL_X signals from FPGA!
+   */
+  typedef component_dac_t INJBIAS_REGULATOR_T;
 
-// FIXME
-// MISSING
-// fast ADC: ADC_IN_XY
+  // FIXME
+  // MISSING
+  // fast ADC: ADC_IN_XY
 
+} // namespace caribou
 
 #endif /* CARIBOU_DATATYPES_H */
