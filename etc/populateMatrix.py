fff8ccfdcf3785bff00fceb4b1a303ba2783e74d
Simon Spannagel <simon.spannagel@cern.ch>
2017-05-19 12:28:22 +0200

    populateatrix: have some parameters to play with, go for single pixels

--- etc/populateMatrix.py
+++ etc/populateMatrix.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
-matrix_file = open("matrix.cfg","w");
-threshold = 15
-countingmode = 0
+countingmode = 1
 testpulse = 0
 longcnt = 1
 
+import sys
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+
+c = sys.argv[1]
+r = sys.argv[2]
+threshold = sys.argv[3]
+
+matrix_file = open("matrix_px" + str(c) + "_" + str(r) + "_cnt_trim" + str(threshold) + ".cfg","w");
+
 matrix_file.write("#Peary CLICpix2 Matrix configuration\n")
-matrix_file.write("# ROWL COL mask threshold countingmode testpulse longcnt\n")
+matrix_file.write("# ROW COL mask threshold countingmode testpulse longcnt\n")
 for row in range(0,128):
     for column in range(0, 128):
-        mask = 0
-        # if row > 10 or column > 10:
-        #     mask = 1
-        matrix_file.write("%i %i %i %i %i %i %i\n" % (row, column, mask, threshold, countingmode, testpulse, longcnt))
+        mask = 1
+        if row == int(r) and column == int(c):
+            print "Unmasked: " + str(c) + "," + str(r)
+            mask = 0
+        matrix_file.write("%i %i %i %i %i %i %i\n" % (row, column, mask, int(threshold), countingmode, testpulse, longcnt))
 
 matrix_file.close()
