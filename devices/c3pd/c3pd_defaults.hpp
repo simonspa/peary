e52c5cb54194e4c9e875973db306086e5f9668fe
Simon Spannagel <simon.spannagel@cern.ch>
2017-05-24 19:20:50 +0200

    C3PD: add all (sub) registers using the register_t mask

--- devices/c3pd/c3pd_defaults.hpp
+++ devices/c3pd/c3pd_defaults.hpp
@@ -60,11 +60,25 @@ namespace caribou {
  */
 #define C3PD_REGISTERS                                                                                                      \
   {                                                                                                                         \
-    {"gcr", register_t<>(0x00)}, {"isg", register_t<>(0x01)}, {"ani", register_t<>(0x02)}, {"ano", register_t<>(0x03)},     \
-      {"vbpre", register_t<>(0x04)}, {"vbpcas", register_t<>(0x05)}, {"vboalf", register_t<>(0x06)},                        \
-      {"vboahf", register_t<>(0x07)}, {"vbls", register_t<>(0x08)}, {"vbfbk", register_t<>(0x09)},                          \
-      {"vbs", register_t<>(0x0A)}, {"vbsp", register_t<>(0x0B)}, {"vbpreoff", register_t<>(0x0C)},                          \
-      {"vblsoff", register_t<>(0x0D)}, {"vbtp", register_t<>(0x0E)},                                                        \
+    {"gcr", register_t<>(0x00)}, {"dlc", register_t<>(0x00, 0x60)}, {"pwrexen", register_t<>(0x00, 0x10)},                  \
+      {"tpexten", register_t<>(0x00, 0x02)}, {"tpen", register_t<>(0x00, 0x01)}, {"isg", register_t<>(0x01)},               \
+      {"pwri", register_t<>(0x01, 0x02)}, {"tpis", register_t<>(0x01, 0x01)}, {"ani", register_t<>(0x02)},                  \
+      {"oir", register_t<>(0x02, 0x80)}, {"ai", register_t<>(0x02, 0x0F)}, {"ano", register_t<>(0x03)},                     \
+      {"pmc", register_t<>(0x03, 0x30)}, {"ao", register_t<>(0x03, 0x0F)}, {"vbpre", register_t<>(0x04)},                   \
+      {"vbpcas", register_t<>(0x05)}, {"vboalf", register_t<>(0x06)}, {"vboahf", register_t<>(0x07)},                       \
+      {"vbls", register_t<>(0x08)}, {"vbfbk", register_t<>(0x09)}, {"vbs", register_t<>(0x0A)},                             \
+      {"vbsp", register_t<>(0x0B)}, {"vbpreoff", register_t<>(0x0C)}, {"vblsoff", register_t<>(0x0D)},                      \
+      {"vbtp", register_t<>(0x0E)}, {"tpcea", register_t<>(0x10)}, {"tpceb", register_t<>(0x11)},                           \
+      {"tpcec", register_t<>(0x12)}, {"tpced", register_t<>(0x13)}, {"tpcee", register_t<>(0x14)},                          \
+      {"tpcef", register_t<>(0x15)}, {"tpceg", register_t<>(0x16)}, {"tpceh", register_t<>(0x17)},                          \
+      {"tpcei", register_t<>(0x18)}, {"tpcej", register_t<>(0x19)}, {"tpcek", register_t<>(0x1A)},                          \
+      {"tpcel", register_t<>(0x1B)}, {"tpcem", register_t<>(0x1C)}, {"tpcen", register_t<>(0x1D)},                          \
+      {"tpceo", register_t<>(0x1E)}, {"tpcep", register_t<>(0x1F)}, {"tprea", register_t<>(0x20)},                          \
+      {"tpreb", register_t<>(0x21)}, {"tprec", register_t<>(0x22)}, {"tpred", register_t<>(0x23)},                          \
+      {"tpree", register_t<>(0x24)}, {"tpref", register_t<>(0x25)}, {"tpreg", register_t<>(0x26)},                          \
+      {"tpreh", register_t<>(0x27)}, {"tprei", register_t<>(0x28)}, {"tprej", register_t<>(0x29)},                          \
+      {"tprek", register_t<>(0x2A)}, {"tprel", register_t<>(0x2B)}, {"tprem", register_t<>(0x2C)},                          \
+      {"tpren", register_t<>(0x2D)}, {"tpreo", register_t<>(0x2E)}, {"tprep", register_t<>(0x2F)},                          \
   }
 
 } // namespace caribou
