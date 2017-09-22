8a0c650df5b5c34e506cb1639c02e629eef2c110
Simon Spannagel <simon.spannagel@cern.ch>
2017-09-22 15:46:58 +0200

    Add auxiliaryDevice which holds an interface but does not use HAL

---
+++ peary/device/auxiliarydevice.hpp
@@ -0,0 +1,17 @@
+/**
+ * Caribou Auxiliary Device header
+ */
+
+#ifndef CARIBOU_DEVICE_AUXILIARY_H
+#define CARIBOU_DEVICE_AUXILIARY_H
+
+#include "configuration.hpp"
+#include "device.hpp"
+
+#include <string>
+#include <vector>
+
+#include "auxiliarydevice.hcc"
+#include "auxiliarydevice.tcc"
+
+#endif /* CARIBOU_DEVICE_AUXILIARY_H */
