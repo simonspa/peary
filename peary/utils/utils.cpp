6d21983c767fbac8cc09bbc0c63320560859f3bc
Simon Spannagel <simon.spannagel@cern.ch>
2018-06-07 11:29:17 +0200

    Update formatting

--- peary/utils/utils.cpp
+++ peary/utils/utils.cpp
@@ -42,9 +42,11 @@ uint8_t caribou::reverseByte(uint8_t n) {
 
   /** Lookup table for bit order reversion
    */
+  // clang-format off
   static unsigned char lookup[16] = {
     0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe, 0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf,
   };
+  // clang-format on
 
   /** Reverse the top and bottom nibble then swap them:
    *   + lookup reverse of bottom nibble
