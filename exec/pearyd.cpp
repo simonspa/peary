9a04294df318dd1feb8ecc12e71efdf5140e400c
Moritz Kiehn <msmk@cern.ch>
2018-07-06 17:39:20 +0200

    pearyd: rework message framing and encoding

--- exec/pearyd.cpp
+++ exec/pearyd.cpp
@@ -2,18 +2,17 @@
 /// \date   2018-07-05
 /// \author Moritz Kiehn <msmk@cern.ch>
 
-#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <cstdint>
-#include <fstream>
-#include <regex>
+#include <string>
 #include <vector>
 
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/uio.h>
 #include <unistd.h>
 
 #include "configuration.hpp"
@@ -63,6 +62,214 @@ void termination_handler(int signal) {
   terminate_success();
 }
 
+// -----------------------------------------------------------------------------
+// message framing over streaming connection
+
+/// Read a fixed number of bytes into the buffer.
+///
+/// \returns true   on sucess
+/// \returns false  upon disconnect of the client
+///
+/// \warning The buffer **must** be big enough to receive the requested data
+bool read_fixed_into(int fd, size_t length, void* buffer)
+{
+  uint8_t* bytes = reinterpret_cast<uint8_t*>(buffer);
+  // keep-alive packages could have zero length payload
+  if (length == 0) {
+    return true;
+  }
+  do {
+    ssize_t ret = read(fd, bytes, length);
+    if (ret < 0) {
+      terminate_errno();
+    }
+    if (ret == 0) {
+      return false;
+    }
+    length -= ret;
+    bytes += ret;
+  } while (0 < length);
+  return true;
+}
+
+/// Read a length-encoded message into the buffer.
+bool read_msg_into(int fd, std::vector<uint8_t>& buffer)
+{
+  uint32_t len;
+
+  // read message length
+  if (!read_fixed_into(fd, 4, &len)) {
+    // client disconnected
+    return false;
+  }
+  // decode message length
+  len = ntohl(len);
+  LOG(DEBUG) << "Request message length=" << len;
+
+  // read message content
+  buffer.resize(len);
+  if (!read_fixed_into(fd, len, buffer.data())) {
+    LOG(ERROR) << "Client disconnected during request";
+    return false;
+  }
+
+  return true;
+}
+
+/// Write a length-encoded message using multiple buffers as input
+///
+/// Each buffer must have a `.data()` and `.size()` member fuctions.
+template <typename... Buffers>
+void write_msg(int fd, Buffers&&... buffers)
+{
+  constexpr size_t n = sizeof...(Buffers);
+
+  // fill scatter/gather lists. one extra element for length
+  std::array<struct iovec, 1 + n> iov = {{
+    { nullptr, 0 },
+    { ((void*)buffers.data()), (buffers.size() * sizeof(*buffers.data())) }...
+  }};
+
+  // compute and check total message length
+  size_t length = 0;
+  for (size_t i = 1; i < (1 + n); ++i) {
+    length += iov[i].iov_len;
+  }
+  if (UINT32_MAX < length) {
+    LOG(ERROR) << "Reply message length > 2^32 - 1";
+    terminate_failure();
+  }
+  LOG(DEBUG) << "Reply message nbuffers=" << n << " length=" << length;
+
+  // encoded message length
+  uint32_t encoded_length = htonl(static_cast<uint32_t>(length));
+  iov[0].iov_base = &encoded_length;
+  iov[0].iov_len = 4;
+
+  // write all message parts in a single step
+  ssize_t ret = writev(fd, iov.data(), iov.size());
+  if (ret == -1) {
+    terminate_errno();
+  }
+  if (static_cast<size_t>(ret) != (4 + length)) {
+    LOG(ERROR) << "Could not write full message";
+    terminate_failure();
+  }
+}
+
+// -----------------------------------------------------------------------------
+// reply message encoding
+
+enum ReplyError : uint8_t {
+  // 0 is not part of this enum since it is not an error
+  MalformedRequest = 1,
+  UnknownCommand = 2,
+};
+
+struct ReplyBuffer {
+  std::array<uint8_t, 5> header;
+  std::string payload;
+
+  void reset() {
+    header.fill(0);
+    payload.clear();
+  }
+  void set_sequence_encoded(const void* buf) {
+      std::memcpy(header.data(), buf, 4);
+  }
+  void set_sequence(uint32_t seq) {
+    *reinterpret_cast<uint32_t*>(header.data()) = htonl(seq);
+  }
+  void set_success() { header[4] = 0; }
+  void set_error(ReplyError err) { header[4] = err; }
+};
+
+/// Split string into substrings using spaces as separator.
+///
+/// Each part is stripped of leading/following whitespace as well.
+std::vector<std::string> split_by_whitespace(const char* txt, size_t len)
+{
+  std::vector<std::string> parts;
+
+  constexpr char sep = ' ';
+  while (0 < len) {
+    // search first occurence of a non-separator byte
+    if (*txt == sep) {
+      txt += 1;
+      len -= 1;
+      continue;
+    }
+    const char* start = txt;
+    // seach next occurence of a separator byte
+    while (0 < len) {
+      if (*txt == sep) {
+        continue;
+      }
+      txt += 1;
+      len -= 1;
+    }
+    // select substring
+    parts.emplace_back(start, txt - start);
+    LOG(DEBUG) << "split part '" << parts.back() << "'";
+  }
+  return parts;
+}
+
+// -----------------------------------------------------------------------------
+// request/reply handling
+
+void process_request(const std::vector<uint8_t>& request, ReplyBuffer& reply) {
+
+  // request **must** contain at least the sequence number
+  if (request.size() < 4) {
+    LOG(ERROR) << "Received malformed request";
+    // no request sequence number is available
+    reply.set_sequence(0);
+    reply.set_error(MalformedRequest);
+    reply.payload = "Malformed request";
+    return;
+  }
+  // empty request is a keep-alive that returns no data
+  if (request.size() == 4) {
+    LOG(DEBUG) << "Received keep-alive";
+    reply.set_sequence_encoded(request.data());
+    reply.set_success();
+    reply.payload.clear();
+    return;
+  }
+
+  // reply **must** always contain the same sequence number as the request
+  // is the same regardless of executed command
+  reply.set_sequence_encoded(request.data());
+
+  // split request into command and arguments
+  const char* cmd = reinterpret_cast<const char*>(request.data() + 4);
+  const char* args = cmd;
+  size_t len_args = request.size() - 4;
+  size_t len_cmd = 0;
+  // find first space to detect end of command
+  for(; (0 < len_args) and (*args != ' '); ++args, --len_args, ++len_cmd) {}
+  // find next non-space to detect beginning of arguments
+  for(; (0 < len_args) and (*args == ' '); ++args, --len_args) {}
+
+  LOG(DEBUG) << "Received command '" << std::string(cmd, len_cmd) << "'";
+  LOG(DEBUG) << "Received arguments '" << std::string(args, len_args) << "'";
+
+  // select commands
+  if (strncmp(cmd, "hello", len_cmd) == 0 ) {
+    reply.set_success();
+    reply.payload = "1";
+  } else {
+    // everything else gives an error
+    reply.set_error(UnknownCommand);
+    reply.payload = "Unknown command: ";
+    reply.payload.append(cmd, len_cmd);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// application loop
+
 void show_help() {
   std::cout << "usage: pearyd [options]\n";
   std::cout << "\n";
@@ -93,74 +300,6 @@ void parse_args(int argc, char* argv[], uint16_t& port, std::string& verbosity)
       terminate_failure();
     }
   }
-}
-
-/// Read a fixed number of bytes into the buffer.
-///
-/// \returns true   on sucess
-/// \returns false  upon disconnect of the client
-///
-/// \warning The buffer **must** be big enough to receive the requested data
-bool read_fixed_into(int fd, uint32_t length, void* buffer)
-{
-  uint8_t* bytes = reinterpret_cast<uint8_t*>(buffer);
-  // keep-alive packages could have zero length payload
-  if (length == 0) {
-    return true;
-  }
-  do {
-    ssize_t ret = read(fd, bytes, length);
-    if (ret < 0) {
-      terminate_errno();
-    }
-    if (ret == 0) {
-      return false;
-    }
-    length -= ret;
-    bytes += ret;
-  } while (0 < length);
-  return true;
-}
-
-/// Decode 8 byte header into sequence number and message length.
-void decode_header(const uint8_t* buffer, uint32_t& sequence, uint32_t& length)
-{
-  sequence = ntohl(*reinterpret_cast<const uint32_t*>(buffer + 0));
-  length = ntohl(*reinterpret_cast<const uint32_t*>(buffer + 4));
-}
-
-// Encode sequence number and message length into 8 byte header.
-void encode_header(uint32_t sequence, uint32_t length, uint8_t* buffer)
-{
-  *reinterpret_cast<uint32_t*>(buffer + 0) = htonl(sequence);
-  *reinterpret_cast<uint32_t*>(buffer + 4) = htonl(length);
-}
-
-// command handling
-
-void process_command(const std::vector<uint8_t>& request, std::string& reply)
-{
-  const char* txt = reinterpret_cast<const char*>(request.data());
-  size_t len = request.size();
-
-  if (strncmp(txt, "hello", len) == 0) {
-    reply = "ok 1";
-  } else if (strncmp(txt, "add_device", len) == 0) {
-    reply = "error not implemented";
-  } else if (strncmp(txt, "list_devices", len) == 0) {
-    reply = "ok";
-  } else if (strncmp(txt, "device", len) == 0) {
-    reply = "error not implemented";
-  } else {
-    reply = "error unknown command";
-  }
-}
-
-std::string command_device(caribou::caribouDevice& dev,
-                           const std::string& command,
-                           const std::vector<std::string>& arguments)
-{
-  return "not implemented";
 }
 
 int main(int argc, char* argv[]) {
@@ -219,8 +358,8 @@ int main(int argc, char* argv[]) {
             << inet_ntoa(server_addr.sin_addr)
             << ":" << ntohs(server_addr.sin_port);
 
-  // we serve only a single client at any given time, but allow multiple
-  // serial connections one after one another.
+  // client loop
+  // only a single client at any time, but multiple connections in series.
   while (true) {
     // wait for a client to connect
     struct sockaddr_in client_addr;
@@ -241,42 +380,20 @@ int main(int argc, char* argv[]) {
               << ":" << ntohs(client_addr.sin_port);
 
     // reusable buffers
-    std::array<uint8_t, 8> header;
-    std::vector<uint8_t> request;
-    std::string reply;
+    std::vector<uint8_t> request_buffer;
+    ReplyBuffer reply_buffer;
 
+    // request/reply loop
     while(true) {
-      // read request header
-      uint32_t sequence, length;
-      if(!read_fixed_into(client_fd, 8, header.data())) {
+
+      if(!read_msg_into(client_fd, request_buffer)) {
         LOG(INFO) << "Client disconnected";
-        // allow next client to connect
-        break;
-      }
-      decode_header(header.data(), sequence, length);
-      LOG(DEBUG) << "Request header sequence=" << sequence << " length=" << length;
-      // read request message
-      request.resize(length);
-      if(!read_fixed_into(client_fd, length, (void*)request.data())) {
-        LOG(ERROR) << "Client disconnected during request";
+         // allow next client to connect
         break;
       }
 
-      process_command(request, reply);
-
-      if(UINT32_MAX < reply.size()) {
-        LOG(ERROR) << "Reply message is larger than 2^32 bytes";
-        terminate_failure();
-      }
-      // send reply header with the same sequence number
-      LOG(DEBUG) << "Request header sequence=" << sequence << " length=" << reply.size();
-      encode_header(sequence, reply.size(), header.data());
-      if(write(client_fd, header.data(), 8) == -1) {
-        terminate_errno();
-      }
-      if(write(client_fd, reply.data(), reply.size()) == -1) {
-        terminate_errno();
-      }
+      process_request(request_buffer, reply_buffer);
+      write_msg(client_fd, reply_buffer.header, reply_buffer.payload);
     }
 
     close(client_fd);
