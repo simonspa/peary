3cceef0e43ff80f7c43d9e8fe2bc987e479109c7
Moritz Kiehn <msmk@cern.ch>
2018-07-06 18:52:05 +0200

    pearyd: update python client

--- python/peary.py
+++ python/peary.py
@@ -8,63 +8,83 @@ import random
 import socket
 import struct
 
-MSG_HEADER = struct.Struct('!LL')
+PROTOCOL_VERSION = b'1'
 
-class SequenceMissmatch(Exception):
-    def __init__(self, request, reply):
-        msg = ('Sequence number missmatch'
-               'request={:} reply{:d}'.format(request, reply))
-        super(SequenceMissmatchError, self).__init__(msg)
+# total length, sequence number
+REQUEST_HEADER = struct.Struct('!LL')
+# total length, sequence number, status code
+REPLY_HEADER = struct.Struct('!LLB')
+
+class UnsupportedProtocol(Exception):
+    pass
 class InvalidReply(Exception):
     pass
 class FailedCommand(Exception):
-    def __init__(self, cmd, reason):
-        super(FailedCommand, self).__init__(
-            'Command \'{}\' failed with error \'{}\''.format(cmd, reason))
+    def __init__(self, cmd, code, reason):
+        msg = 'Command \'{}\' failed with error {:d} \'{}\''
+        super(FailedCommand, self).__init__(msg.format(cmd, code, reason))
 
 class PearyClient(object):
+    """
+    Connect to a pearyd instance running somewhere else.
+    """
     def __init__(self, host, port=12345):
         super(PearyClient, self).__init__()
         self.host = host
         self.port = port
-        self._socket = socket.create_connection((self.host, self.port))
         self._sequence = random.Random()
         self._sequence.seed()
+        self._socket = socket.create_connection((self.host, self.port))
+        # check connection and protocol
+        version = self._request('hello')
+        if version != PROTOCOL_VERSION:
+            raise UnsupportedProtocol(version)
 
-    def _command(self, cmd):
+    def _request(self, cmd):
+        # randint limits are inclusive
         req_seq = self._sequence.randint(0, 2**32 - 1)
         req_payload = cmd.encode('utf-8')
-        req_header = MSG_HEADER.pack(req_seq, len(req_payload))
+        req_header = REQUEST_HEADER.pack(4 + len(req_payload), req_seq)
         self._socket.send(req_header)
         self._socket.send(req_payload)
-        rep_header = self._socket.recv(8)
-        rep_seq, rep_len = MSG_HEADER.unpack(rep_header)
+        rep_header = self._socket.recv(9)
+        rep_len, rep_seq, rep_err = REPLY_HEADER.unpack(rep_header)
+        if rep_len < 5:
+            raise InvalidReply('Length is too small')
         if rep_seq != req_seq:
-            raise SequenceMissmatch(req_seq, rep_seq)
-        rep_payload = self._socket.recv(rep_len)
-        if rep_payload.startswith(b'ok'):
-            return rep_payload[3:]
-        elif rep_payload.startswith(b'error'):
-            raise FailedCommand(cmd, rep_payload[6:].decode('ascii'))
-        else:
-            raise InvalidReply(rep_payload)
+            raise InvalidReply('Sequence number missmatch', req_seq, rep_seq)
+        rep_payload = self._socket.recv(rep_len - 5)
+        if rep_err:
+            raise FailedCommand(cmd, rep_err, rep_payload.decode('utf-8'))
+        return rep_payload
 
+    def keep_alive(self):
+        self._request('')
+
+    def list_devices(self):
+        indices = self._request('list_devices')
+        indices = [int(_) for _ in indices.split()]
+        return [Device(_) for _ in indices]
     def add_device(self, name):
-        index = self._command('add_device {}'.format(name))
+        index = self._request('add_device {}'.format(name))
         index = int(index)
-        return Device(self, index)
-    def get_device(self, index):
         return Device(self, index)
 
 class Device(object):
+    """
+    A Peary device.
+
+    This acts as a proxy object that just forwards all function calls via the
+    network.
+    """
     def __init__(self, client, index):
         super(Device, self).__init__()
         self._client = client
-        self._prefix = 'device {:d}'.format(index)
+        self._prefix = 'device.{:d}.'.format(index)
     def _call(self, name, *args):
         parts = [self._prefix, name]
         parts.extend(str(_) for _ in args)
-        return self._client._command(' '.join(parts))
+        return self._client._request(' '.join(parts))
     def __getattr__(self, name):
         func = functools.partial(self._call, name)
         self.__dict__[name] = func
@@ -72,8 +92,7 @@ class Device(object):
 
 if __name__ == '__main__':
     pc = PearyClient(host='localhost')
-    print(pc._command('hello'))
-    print(pc._command('list_devices'))
-    # print(pc._command('device 1 get_name'))
-    dev0 = pc.get_device(0)
-    print(dev0.get_name())
+    pc.keep_alive()
+    print(pc.list_devices())
+    dev1 = pc.add_device('Example')
+    print(dev1)
