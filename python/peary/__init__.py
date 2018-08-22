a7125c42b8d4bfcbf850f562946a7508a7288130
Moritz Kiehn <msmk@cern.ch>
2018-08-22 14:31:07 +0200

    python: document all public methods

--- python/peary/__init__.py
+++ python/peary/__init__.py
@@ -102,13 +102,22 @@ class PearyClient(object):
         return rep_payload
 
     def keep_alive(self):
+        """
+        Send a keep-alive message to test the connection.
+        """
         self._request('')
 
     def list_devices(self):
+        """
+        List configured devices.
+        """
         indices = self._request('list_devices')
         indices = [int(_) for _ in indices.split()]
         return [self.get_device(_) for _ in indices]
     def get_device(self, index):
+        """
+        Get the device object corresponding to the given index.
+        """
         device = self._devices.get(index)
         if not device:
             device = self._devices.setdefault(index, Device(self, index))
@@ -163,38 +172,53 @@ class Device(object):
     # additional return value decoding where appropriate
 
     def power_on(self):
+        """Power on the device."""
         self._request('power_on')
     def power_off(self):
+        """Power off the device."""
         self._request('power_off')
     def reset(self):
+        """Reset the device."""
         self._request('reset')
     def configure(self):
+        """Initialize and configure the device."""
         self._request('configure')
     def daq_start(self):
+        """Start data aquisition for the device."""
         self._request('daq_start')
     def daq_stop(self):
+        """Stop data aquisition for the device."""
         self._request('daq_stop')
 
     def list_registers(self):
+        """List all available registers by name."""
         return self._request('list_registers').decode('utf-8').split()
     def get_register(self, name):
+        """Get the value of a named register."""
         return int(self._request('get_register', name))
     def set_register(self, name, value):
+        """Set the value of a named register."""
         self._request('set_register', name, value)
 
     def get_current(self, name):
+        """Get the measured current of a named periphery port."""
         return float(self._request('get_current', name))
     def set_current(self, name, value):
+        """Set the current of a named periphery port."""
         self._request('set_current', name, value)
 
     def get_voltage(self, name):
+        """Get the measured voltage of a named periphery port."""
         return float(self._request('get_voltage', name))
     def set_voltage(self, name, value):
+        """Set the voltage of a named periphery port."""
         self._request('set_voltage', name, value)
 
     def switch_on(self, name):
+        """Switch on a periphery port."""
         self._request('switch_on', name)
     def switch_off(self, name):
+        """Switch off a periphery port."""
         self._request('switch_off', name)
 
     # unknown attributes are interpreted as dynamic functions
