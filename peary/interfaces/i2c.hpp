5b69681ac0641123a340559b7bc096a509dd52d0
Simon Spannagel <simon.spannagel@cern.ch>
2019-03-31 16:33:59 +0200

    Improve code documentation for I2C interface

--- peary/interfaces/i2c.hpp
+++ peary/interfaces/i2c.hpp
@@ -1,9 +1,7 @@
 #ifndef CARIBOU_HAL_I2C_HPP
 #define CARIBOU_HAL_I2C_HPP
 
-#include <cstdint>
 #include <mutex>
-#include <string>
 #include <vector>
 
 #include "exceptions.hpp"
@@ -16,30 +14,45 @@ namespace caribou {
   typedef uint8_t i2c_t;
   typedef uint8_t i2c_reg_t;
 
+  /**
+   * @ingroup Interfaces
+   * @brief I2C interface via Kernel I2C module
+   */
   class iface_i2c : public Interface<i2c_t, i2c_reg_t, i2c_t> {
-
   private:
-    // Default constructor: private (only created by interface_manager)
-    //
-    // It can throw DeviceException
+    /**
+     * @brief Private constructor, only to be created by the interface manager. Opens the file descriptor for the I2C module
+     * @param device_path Path the device listens at
+     * @throws DeviceException if device cannot be reached
+     */
     iface_i2c(std::string const& device_path);
 
-    virtual ~iface_i2c();
+    /**
+     * @brief Default constructor deleted, cannot have I2C interface without bus address
+     */
+    iface_i2c() = delete;
 
-    // Set I2C address before read/write access
-    //
-    // It can throw CommunicationError
+    /**
+     * @brief Destructor closes the file handle for the I2C module
+     */
+    ~iface_i2c();
+
+    /**
+     * @brief Sets endpoint address before communication
+     * @param address I2C address to address
+     * @throws CommunicationError if device cannot be contacted
+     */
     inline void setAddress(i2c_address_t const address);
 
-    // Descriptor of the device
+    // File descriptor for the I2C module
     int i2cDesc;
 
     // Protects access to the bus
     std::mutex mutex;
 
+    // caribouHAL is allowed to access private members for reading and writing
     template <typename T> friend class caribouHAL;
 
-  private:
     i2c_t write(const i2c_address_t& address, const i2c_t& data);
     std::pair<i2c_reg_t, i2c_t> write(const i2c_address_t& address, const std::pair<i2c_reg_t, i2c_t>& data);
     std::vector<i2c_t> write(const i2c_address_t& address, const i2c_reg_t& reg, const std::vector<i2c_t>& data);
@@ -52,8 +65,6 @@ namespace caribou {
     // Special functions to read/write to devices with up to 16bit register
     std::vector<i2c_t> wordwrite(const i2c_address_t& address, const uint16_t& reg, const std::vector<i2c_t>& data);
     std::vector<i2c_t> wordread(const i2c_address_t& address, const uint16_t reg, const unsigned int length = 1);
-    // Unused constructor
-    iface_i2c() = delete;
 
     // only this function can create the interface
     friend iface_i2c& interface_manager::getInterface<iface_i2c>(std::string const&);
