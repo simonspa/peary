d4d99b04cff03027ce8bc92a4e6ae245c47d460a
Simon Spannagel <simon.spannagel@cern.ch>
2019-05-02 09:44:43 +0200

    Pixel: add copy/move operators

--- peary/utils/datatypes.hpp
+++ peary/utils/datatypes.hpp
@@ -22,8 +22,27 @@ namespace caribou {
    */
   class pixel {
   public:
-    pixel(){};
-    virtual ~pixel(){};
+    pixel() = default;
+    /**
+     * @brief Required virtual destructor
+     */
+    virtual ~pixel() = default;
+
+    /// @{
+    /**
+     * @brief Use default copy behaviour
+     */
+    pixel(const pixel&) = default;
+    pixel& operator=(const pixel&) = default;
+    /// @}
+
+    /// @{
+    /**
+     * @brief Use default move behaviour
+     */
+    pixel(pixel&&) = default;
+    pixel& operator=(pixel&&) = default;
+    /// @}
 
     /** Overloaded ostream operator for printing of pixel data
      */
