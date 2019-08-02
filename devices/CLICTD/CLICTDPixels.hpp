0b7f7287fc483fadc9a7629db4177dda327c3291
Simon Spannagel <simon.spannagel@cern.ch>
2019-08-02 15:00:29 +0200

    CLICTD pixels: split tages for configuration to 2x22

--- devices/CLICTD/CLICTDPixels.hpp
+++ devices/CLICTD/CLICTDPixels.hpp
@@ -11,7 +11,7 @@ namespace caribou {
 
   // Basic pixel class
   // The information is internally stored in the same way, the chip stores it, as
-  // a 44bit register.
+  // a 22bit register.
   //
   // The individual values are set via the member functions of a specialized classes
   class clictd_pixel : public virtual pixel {
@@ -19,7 +19,7 @@ namespace caribou {
     virtual ~clictd_pixel(){};
 
     // direct latch access
-    void setLatches(uint64_t latches) { m_latches = latches; }
+    void setLatches(uint32_t latches) { m_latches = latches; }
 
     // set a dedicated bit of the latch
     void setLatches(bool bit, uint8_t idx) {
@@ -33,7 +33,7 @@ namespace caribou {
      */
     bool GetBit(uint8_t bit) { return ((m_latches >> bit) & 0x1); }
 
-    uint16_t GetLatches() const { return m_latches; }
+    uint32_t GetLatches() const { return m_latches; }
 
     /** Overloaded comparison operators
      */
@@ -49,45 +49,78 @@ namespace caribou {
   protected:
     // Set default 1-bits:
     clictd_pixel(){};
-    clictd_pixel(uint64_t m_latches) : m_latches(m_latches){};
-    uint64_t m_latches;
+    clictd_pixel(uint32_t m_latches) : m_latches(m_latches){};
+    uint32_t m_latches;
   };
 
   /* CLICTD pixel configuration class
    *
-   * Class to hold all information required to fully configure one pixel.
-   * The information is internally stored in the same way, the chip stores it, as
-   * a 44bit register. The individual values are set via the member functions
-   * and can be retrieved bitwise for convenience.
+   * Class to hold all information required to configure one pixel.
+   * The information is internally stored in the same way the chip stores it, i.e. as two separate write commands to the
+   * pixel latches of 22bits each
+   * The individual values are set via the member functions and can be retrieved bitwise for convenience.
    *
-   * Layout:
-   * ( 0 ) ( stage 2 ) ( stage 1 )
-   * i.e.
-   * ( 0 )
+   * Layout Stage 2:
    * ( 1 tDAC7[3] tDAC6[3] tDAC5[3] tDAC4[3] tDAC3[3] tDAC2[3] tDAC1[2] 0 )
+   *
+   * Layout Stage 1:
    * ( 1 tDAC1[1] tDAC0[3] tpEnA[8] mask[8] tpEnD[1] )
    */
   class pixelConfig : public virtual clictd_pixel {
   public:
     /* Default constructor
      *
-     * Initializes the pixel in masked state
+     * Initializes the pixel configuration with bit 21 always set to '1'
      */
-    pixelConfig() : clictd_pixel(0x800002001FE){};
-    pixelConfig(uint8_t mask, bool tp_digital, uint8_t tp_analog, std::vector<uint8_t> thresholds) : pixelConfig() {
+    pixelConfig() : clictd_pixel(0x00200000){};
+    pixelConfig(uint32_t latches) : clictd_pixel(latches){};
+  };
+
+  /* CLICTD Stage1 pixel configuration class
+   *
+   * Class to hold all information required to configure one pixel.
+   * The information is internally stored in the same way the chip stores it, i.e. as two separate write commands to the
+   * pixel latches of 22bits each
+   * The individual values are set via the member functions and can be retrieved bitwise for convenience.
+   *
+   * Layout Stage 2:
+   * ( 1 tDAC7[3] tDAC6[3] tDAC5[3] tDAC4[3] tDAC3[3] tDAC2[3] tDAC1[2] 0 )
+   *
+   * Layout Stage 1:
+   * ( 1 tDAC1[1] tDAC0[3] tpEnA[8] mask[8] tpEnD[1] )
+   */
+  class pixelConfigStage1 : public pixelConfig {
+  public:
+    /**
+     * Constructor for a stage 1 configuration data object:
+     * @param mask       Mask bits for analog front ends
+     * @param tp_digital Enable digital test pulsing
+     * @param tp_analog  Test pulse enable bits for analog front-ends
+     * @param thresholds Threshold tuning DAC settings for channels 0-7 (front-ends 2-7 are ignored)
+     */
+    pixelConfigStage1(uint8_t mask, bool tp_digital, uint8_t tp_analog, std::vector<uint8_t> thresholds) : pixelConfig() {
       SetMask(mask);
       EnableTestpulseDigital(tp_digital);
       EnableTestpulseAnalog(tp_analog);
-      SetThresholds(thresholds);
-    };
 
+      if(thresholds.size() >= 2) {
+        SetThreshold(0, thresholds.at(0));
+        SetThreshold(1, thresholds.at(1));
+      } else {
+        throw ConfigInvalid("Wrong number of threshold adjustment settings");
+      }
+    }
+
+    // By default, instantiated as masked
+    pixelConfigStage1() : pixelConfig(0x002001FE){};
+
+  private:
     /* Mask setting of the sub-pixels
      */
     void SetMask(uint8_t mask) {
       uint64_t bit_mask = (0xFF << 1); // Bitmask where the analog frontend masks are located
       m_latches = (m_latches & (~bit_mask)) | (mask << 1);
     }
-    uint8_t GetMask() const { return (m_latches >> 1) & 0xFF; }
 
     void SetThreshold(uint8_t frontend, uint8_t threshold) {
       // Strip everything but three bits:
@@ -102,45 +135,7 @@ namespace caribou {
         } else {
           m_latches &= ~(0x1 << 20);
         }
-        uint64_t bit_mask = (0x06 << 22);
-        m_latches = (m_latches & (~bit_mask)) | ((value & 0x06) << 22);
-      } else {
-        size_t shift = (frontend - 2) * 3 + 25;
-        uint64_t bit_mask = (0x07 << shift);
-        m_latches = (m_latches & (~bit_mask)) | (value << shift);
       }
-    }
-
-    /* Individual threshold adjustment (3bit per analog front-end)
-     */
-    void SetThresholds(std::vector<uint8_t> thr_adjust) {
-      if(thr_adjust.size() != 8) {
-        throw ConfigInvalid("Wrong number of threshold adjustments, expecting 8 frontends");
-      }
-
-      for(size_t frontend = 0; frontend < 8; frontend++) {
-        SetThreshold(frontend, thr_adjust.at(frontend));
-      }
-    }
-
-    uint8_t GetThreshold(uint8_t frontend) const {
-      if(frontend >= 8) {
-        throw ConfigInvalid("Frontend does not exist: " + std::to_string(frontend));
-      }
-      if(frontend == 0) {
-        return (m_latches >> 17) & 0x07;
-      } else if(frontend == 1) {
-        return (m_latches >> 22) & 0x06 || (m_latches >> 20) & 0x01;
-      } else {
-        return (m_latches >> ((frontend - 2) * 3 + 25)) & 0x07;
-      }
-    }
-    std::vector<uint8_t> GetThresholds() const {
-      std::vector<uint8_t> thresholds;
-      for(size_t frontend = 0; frontend < 8; frontend++) {
-        thresholds.push_back(GetThreshold(frontend));
-      }
-      return thresholds;
     }
 
     /* Enable/disable testpulse circuit for this pixel
@@ -151,22 +146,82 @@ namespace caribou {
       else
         m_latches &= ~0x1;
     }
-    bool GetEnableTestpulseDigital() const { return m_latches & 0x1; }
-
     /* Enable/disable testpulse circuit for this pixel
-     */
+    */
     void EnableTestpulseAnalog(uint8_t tpen) {
       uint64_t bit_mask = (0xFF << 9); // Bitmask where the analog testpulse bits are located
       m_latches = (m_latches & (~bit_mask)) | (tpen << 9);
     }
+
+  public:
+    uint8_t GetMask() const { return (m_latches >> 1) & 0xFF; }
+    bool GetEnableTestpulseDigital() const { return m_latches & 0x1; }
     uint8_t GetEnableTestpulseAnalog() const { return (m_latches >> 9) & 0xFF; }
 
     /** Overloaded print function for ostream operator
      */
     void print(std::ostream& out) const {
       out << "px [" << this->GetMask() << "|" << this->GetEnableTestpulseDigital() << "-" << this->GetEnableTestpulseAnalog()
-          << "|" << listVector(this->GetThresholds()) << "]";
+          << "|xxxxxxxx]";
     }
+  };
+
+  /* CLICTD Stage2 pixel configuration class
+   *
+   * Layout Stage 2:
+   * ( 1 tDAC7[3] tDAC6[3] tDAC5[3] tDAC4[3] tDAC3[3] tDAC2[3] tDAC1[2] 0 )
+   */
+  class pixelConfigStage2 : public pixelConfig {
+  public:
+    /**
+     * Constructor for a stage 2 configuration data object:
+     * @param thresholds Threshold tuning DAC settings for channels 0-7 (front-end 0 is ignored)
+     */
+    pixelConfigStage2(std::vector<uint8_t> thresholds) : pixelConfig() {
+      if(thresholds.size() < 7 || thresholds.size() > 8) {
+        throw ConfigInvalid("Wrong number of threshold adjustment settings");
+      }
+
+      // Ignore the very first entry if all eicht are shipped:
+      for(size_t frontend = 1; frontend < thresholds.size(); frontend++) {
+        SetThreshold(frontend, thresholds.at(frontend));
+      }
+    };
+
+    pixelConfigStage2() : pixelConfig(){};
+
+  private:
+    void SetThreshold(uint8_t frontend, uint8_t threshold) {
+      // Strip everything but three bits:
+      auto value = threshold & 0x07;
+      if(frontend == 1) {
+        uint64_t bit_mask = (0x06 << 1);
+        m_latches = (m_latches & (~bit_mask)) | ((value & 0x06) << 1);
+      } else {
+        size_t shift = (frontend - 1) * 3;
+        uint64_t bit_mask = (0x07 << shift);
+        m_latches = (m_latches & (~bit_mask)) | (value << shift);
+      }
+    }
+
+  public:
+    uint8_t GetThreshold(uint8_t frontend) const {
+      if(frontend >= 8 || frontend <= 1) {
+        throw ConfigInvalid("Frontend does not exist: " + std::to_string(frontend));
+      }
+      return (m_latches >> ((frontend - 1) * 3)) & 0x07;
+    }
+    std::vector<uint8_t> GetThresholds() const {
+      std::vector<uint8_t> thresholds;
+      for(size_t frontend = 2; frontend < 8; frontend++) {
+        thresholds.push_back(GetThreshold(frontend));
+      }
+      return thresholds;
+    }
+
+    /** Overloaded print function for ostream operator
+     */
+    void print(std::ostream& out) const { out << "px [/|/-//|xx" << listVector(this->GetThresholds()) << "]"; }
   };
 } // namespace caribou
 
