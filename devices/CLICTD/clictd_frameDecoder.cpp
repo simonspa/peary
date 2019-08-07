c2b1a2df36e088e5d0fadec73da0c3f24a6026da
Tomas Vanat <tomas.vanat@cern.ch>
2019-08-07 16:51:57 +0200

    CLICTD frame decoder: removed duplicite definitions of constants

--- devices/CLICTD/clictd_frameDecoder.cpp
+++ devices/CLICTD/clictd_frameDecoder.cpp
@@ -1,11 +1,4 @@
-//#include "clictd_frame.hpp"
-#define CLICTD_FRAME_START 0b1111111111111110101000
-#define CLICTD_FRAME_END 0b1111111111111110010100
-#define CLICTD_COLUMN_ID 0b1111111111111101000000
-#define CLICTD_COLUMN_ID_MASK 0b0000000000000000111100
-#define CLICTD_PIXEL_BITS 22
-#define CLICTD_COLUMNS 16
-#define CLICTD_ROWS 128
+#include "clictd_frame.hpp"
 
 uint32_t getNextPixel(const std::vector<uint32_t>& rawFrame, unsigned* word, unsigned* bit) {
 
